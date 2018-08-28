480533f904eea4e357af51d001bc61e385835faf
Seán Kelly <daxorinator@gmail.com>
2018-08-28 12:11:12 +0100

    Removing useless files from pre-rewrite

--- PokerGame/decks.cpp
+++
@@ -1,8 +0,0 @@
-void card::generateCard() {
-
-}
-
-void card::printDetails() {	
-	static const char* suitStrings[] = {"Hearts", "Diamonds", "Spades", "Clubs"};
-	std::cout << static_cast<int>(this->value) + 1 << " of " << suitStrings[static_cast<int>(this->suit)] << std::endl;
-}
